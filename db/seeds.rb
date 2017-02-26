1c7796bb21c3f760d5f12c1bd56f75edaa3af0ba
Phil Wilson <pjwilson27@gmail.com>
2017-02-26 19:28:34 +0000

    Completed assignment17

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,7 @@ posts = Post.all
         body: RandomData.random_paragraph
         )
     end
+Post.find_or_create_by(title: "Hoping this works", body: "Huzzahs!")
     
 puts "Seed finished"
 puts "#{Post.count} posts created"
