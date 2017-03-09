7c83a0874d3e5295bb4bee200bd2706fe65037ce
Phil Wilson <pjwilson27@gmail.com>
2017-03-09 22:40:49 +0000

    Completed, display error

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) { Question.new(title:"Enter your title", body: "Type your question here...", resolved: false) }
+  
+  describe "attributes" do
+    it "responds to title" do
+        expect(question).to respond_to(:title)
+    end
+    
+    it "responds to body" do
+        expect(question).to respond_to(:body)
+    end
+    
+    it "responds to resolved" do
+        expect(question).to respond_to(:resolved)
+    end
+  end
+end
