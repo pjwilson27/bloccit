0b8d34de745e1bf21096a6fedbaeee8421940a7b
Phil Wilson <pjwilson27@gmail.com>
2017-03-20 20:06:39 +0000

    Pre-help @ db/seeds portion

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Post, type: :model do
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:comments) }
+  it { is_expected.to have_many(:votes) }
   
   it {is_expected.to validate_presence_of(:title) }
   it {is_expected.to validate_presence_of(:body) }
@@ -28,5 +29,32 @@ RSpec.describe Post, type: :model do
       it "has title,body, and user attributes" do
           expect(post).to have_attributes(title: title, body: body, user: user)
       end
+  end
+  
+  describe "voting" do
+    before do
+      3.times { post.votes.create!(value: 1) }
+      2.times { post.votes.create!(value: -1) }
+      @up_votes = post.votes.where(value: 1).count
+      @down_votes = post.votes.where(value: -1).count
+    end
+    
+    describe "upvotes" do
+      it "counts the number of the votes with a value of 1" do
+        expect( post.up_votes ).to eq(@up_votes)
+      end
+    end
+    
+    describe "downvotes" do
+      it "counts the number of votes with a value of -1" do
+        expect( post.down_votes ).to eq(@down_votes)
+      end
+    end
+    
+    describe "points" do
+      it "returns the sum of all down and up votes" do
+        expect( post.points ).to eq(@up_votes - @down_votes)
+      end
+    end
   end
 end
