9a9dd2a046543e2b601232f09af7bb14f281a2bc
Phil Wilson <pjwilson27@gmail.com>
2017-03-20 01:07:20 +0000

    Completed CP28

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,11 +39,20 @@ posts = Post.all
         )
     end
     
-user = User.first
-user.update_attributes!(
-    email: 'bloccit.com',
+#create admin user
+admin = User.create!(
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: 'helloworld',
+    role: 'admin'
+)
+
+#create a member
+member = User.create!(
+    name: 'Member User',
+    email: 'member@example.com',
     password: 'helloworld'
-)    
+)
     
 puts "Seed finished"
 puts "#{User.count} users created"
