37f03ceb374624fea51ddc32b3007371afd54266
Phil Wilson <pjwilson27@gmail.com>
2017-03-24 17:32:51 +0000

    Fixed all errors, page is fully functional

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,18 @@ Rails.application.routes.draw do
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
+#    resources :votes, only: [] do
+#      collection do
+#        post :up_vote
+#        post :down_vote
+#      end
+#    end
   end
   
-  post '/up-vote' => 'votes#up_vote', as: :up_vote
-  post '/down-vote' => 'votes#down_vote', as: :down_vote
+ # post '/up-vote' => 'votes#up_vote', as: :up_vote
+#  post '/down-vote' => 'votes#down_vote', as: :down_vote
   
   get 'about' => 'welcome#about'
   
