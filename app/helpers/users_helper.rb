d74d20430bb53ad9152f5883ce893a99c7c5a54b
Phil Wilson <pjwilson27@gmail.com>
2017-03-28 22:35:46 +0000

    Still working out some bugs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+=begin    
+    def detect_post
+        if user == user.posts
+            flash[:alert] = "#{user.name} has a total of #{post.count} posts on Bloccit!"
+        else
+            flash[:alert] = "#{user.name} hasn't made any posts!!"
+        end
+    end
+    
+    def detect_comment
+        if user == user.comments
+            flash[:alert] = "#{user.name} has a total of #{comment.count} comments throughout Bloccit!"
+        else
+            flash[:alert] = "#{user.name} hasn't got much to say about people's posts..."
+        end
+    end
+    
+    user == @user
+=end 
 end
