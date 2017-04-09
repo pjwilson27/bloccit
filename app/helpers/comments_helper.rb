cc12afab5e03ab419d6898e886597e54104f0b28
Phil Wilson <pjwilson27@gmail.com>
2017-04-09 15:15:51 +0000

    Stuck.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,12 @@ module CommentsHelper
     def user_is_authorized_for_comment?(comment)
         current_user && (current_user == comment.user || current_user.admin?)
     end
+    
+    def user_commented
+        if current_user.comments.count > 0
+            #display the posts
+        else
+            puts "{user.name} has not commented yet."
+        end
+    end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,12 @@ module PostsHelper
     def user_is_authorized_for_post?(post)
         current_user && (current_user == post.user || current_user.admin?)
     end
+    
+    def user_posted
+        if current_user.posts.count > 0
+            #display user posts
+        else
+            puts "{user.name} has not submitted any posts yet."
+        end
+    end
 end
